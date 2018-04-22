377547056c9ee582914898f268caeaa9094a0b6e
Mendy <mendybeltcohen@gmail.com>
2018-04-22 19:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
  function updateObjectWithKeyAndValue(object, key, value) {
-   var newobj = object.assign({}, object.key value);
+   var newobj = object.assign({}, object);
  }
   
