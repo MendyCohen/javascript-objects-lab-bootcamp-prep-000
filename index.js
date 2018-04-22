2a3cabc0e82a9da0bc2af961a1b7b502ac6062cb
Mendy <mendybeltcohen@gmail.com>
2018-04-22 19:37:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var recipes = {};
 //var newobj = {};
    
 
-/* function updateObjectWithKeyAndValue(object, key, value) {
+ function updateObjectWithKeyAndValue(object, key, value) {
   return object.assign({}, object,{key:['value', 'yep']});
- } */
-  
- var updateObjectWithKeyAndValue = {object: 'vlaue'} 
-  return object.assign({}, object,{key:['value']});
- 
+ } 
