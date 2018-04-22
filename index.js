cb146f4ee6708024af0f5afd0cb11d14add831e4
Mendy <mendybeltcohen@gmail.com>
2018-04-22 19:59:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-   Object.assign({}, delete object.key);
+  return Object.assign({}, delete object.key);
 }
